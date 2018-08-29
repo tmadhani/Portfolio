8732f1b6726c2729c4df49e0a340bbf53011713e
tmadhani <tmadhani@umich.edu>
2018-08-28 22:59:11 -0400

    Changed website

--- js/main.js
+++ js/main.js
@@ -1,174 +1,274 @@
-(function(){
-	//remove no-js class
-	removeClass(document.getElementsByTagName("html")[0], "no-js");
 
-	function HeroSlider( element ) {
-		this.element = element;
-		this.navigation = this.element.getElementsByClassName("js-cd-nav")[0];
-		this.navigationItems = this.navigation.getElementsByTagName('li');
-		this.marker = this.navigation.getElementsByClassName("js-cd-marker")[0];
-		this.slides = this.element.getElementsByClassName("js-cd-slide");
-		this.slidesNumber = this.slides.length;
-		this.newSlideIndex = 0;
-		this.oldSlideIndex = 0;
-		this.autoplay = hasClass(this.element, "js-cd-autoplay");
-		this.autoPlayId;
-		this.autoPlayDelay = 5000;
-		this.init();
-	};
+var colors = new Array(
+  [176, 173, 104],
+  [220, 207, 153],
+  [206, 164, 119],
+  [219, 155, 122],
+  [212, 129, 105],
+  [113, 143, 211]);
 
-	HeroSlider.prototype.init = function() {
-		var self = this;
-		//upload video (if not on mobile devices)
-		this.uploadVideo();
-		//autoplay slider
-		this.setAutoplay();
-		//listen for the click event on the slider navigation
-		this.navigation.addEventListener('click', function(event){
-			if( event.target.tagName.toLowerCase() == 'div' )
-				return;
-			event.preventDefault();
-			var selectedSlide = event.target;
-			if( hasClass(event.target.parentElement, 'cd-selected') )
-				return;
-			self.oldSlideIndex = self.newSlideIndex;
-			self.newSlideIndex = Array.prototype.indexOf.call(self.navigationItems, event.target.parentElement);
-			self.newSlide();
-			self.updateNavigationMarker();
-			self.updateSliderNavigation();
-			self.setAutoplay();
-		});
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
 
-		if(this.autoplay) {
-			// on hover - pause autoplay
-			this.element.addEventListener("mouseenter", function(){
-				clearInterval(self.autoPlayId);
-			});
-			this.element.addEventListener("mouseleave", function(){
-				self.setAutoplay();
-			});
-		}
-	};
+//transition speed
+var gradientSpeed = 0.002;
 
-	HeroSlider.prototype.uploadVideo = function() {
-		var videoSlides = this.element.getElementsByClassName("js-cd-bg-video");
-		for( var i = 0; i < videoSlides.length; i++) {
-			if( videoSlides[i].offsetHeight > 0 ) {
-				// if visible - we are not on a mobile device 
-				var videoUrl = videoSlides[i].getAttribute("data-video");
-				videoSlides[i].innerHTML = "<video loop><source src='"+videoUrl+".mp4' type='video/mp4' /><source src='"+videoUrl+".webm' type='video/webm'/></video>";
-				// if the visible slide has a video - play it
-				if( hasClass(videoSlides[i].parentElement, "cd-hero__slide--selected") ) videoSlides[i].getElementsByTagName("video")[0].play();
-			}
-		}
-	};
+function updateGradient()
+{
+  
+  if ( $===undefined ) return;
+  
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
 
-	HeroSlider.prototype.setAutoplay = function() {
-		var self = this;
-		if(this.autoplay) {
-			clearInterval(self.autoPlayId);
-			self.autoPlayId = window.setInterval(function(){self.autoplaySlider()}, self.autoPlayDelay);
-		}
-	};
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
 
-	HeroSlider.prototype.autoplaySlider = function() {
-		this.oldSlideIndex = this.newSlideIndex;
-		var self = this;
-		if( this.newSlideIndex < this.slidesNumber - 1) {
-			this.newSlideIndex +=1;
-			this.newSlide();
-			
-		} else {
-			this.newSlideIndex = 0;
-			this.newSlide();
-		}
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
 
-		this.updateNavigationMarker();
-		this.updateSliderNavigation();
-	};
+ $('#gradient').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+  
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    
+  }
+}
 
-	HeroSlider.prototype.newSlide = function(direction) {
-		var self = this;
-		removeClass(this.slides[this.oldSlideIndex], "cd-hero__slide--selected cd-hero__slide--from-left cd-hero__slide--from-right");
-		addClass(this.slides[this.oldSlideIndex], "cd-hero__slide--is-moving");
-		setTimeout(function(){removeClass(self.slides[self.oldSlideIndex], "cd-hero__slide--is-moving");}, 500);
+setInterval(updateGradient,10);
 
-		for(var i=0; i < this.slidesNumber; i++) {
-			if( i < this.newSlideIndex && this.oldSlideIndex < this.newSlideIndex) {
-				addClass(this.slides[i], "cd-hero__slide--move-left");
-			} else if( i == this.newSlideIndex && this.oldSlideIndex < this.newSlideIndex) {
-				addClass(this.slides[i], "cd-hero__slide--selected cd-hero__slide--from-right");
-			} else if(i == this.newSlideIndex && this.oldSlideIndex > this.newSlideIndex) {
-				addClass(this.slides[i], "cd-hero__slide--selected cd-hero__slide--from-left");
-				removeClass(this.slides[i], "cd-hero__slide--move-left");
-			} else if( i > this.newSlideIndex && this.oldSlideIndex > this.newSlideIndex ) {
-				removeClass(this.slides[i], "cd-hero__slide--move-left");
-			}
-		}
+//SlideToggle javascript
 
-		this.checkVideo();
+/*! Visit www.menucool.com for source code, other menu scripts and web UI controls
+*  Please keep this notice intact. Thank you. */
 
-	};
+var sse1 = function () {
+    var rebound = 20; //set it to 0 if rebound effect is not prefered
+    var slip, k;
+    return {
+        buildMenu: function () {
+            var m = document.getElementById('sses1');
+            if(!m) return;
+            var ul = m.getElementsByTagName("ul")[0];
+            m.style.width = ul.offsetWidth+1+"px";
+            var items = m.getElementsByTagName("li");
+            var a = m.getElementsByTagName("a");
 
-	HeroSlider.prototype.updateNavigationMarker = function() {
-		removeClassPrefix(this.marker, 'item');
-		addClass(this.marker, "cd-hero__marker--item-"+ (Number(this.newSlideIndex) + 1));
-	};
+            slip = document.createElement("li");
+            slip.className = "highlight";
+            ul.appendChild(slip);
 
-	HeroSlider.prototype.updateSliderNavigation = function() {
-		removeClass(this.navigationItems[this.oldSlideIndex], 'cd-selected');
-		addClass(this.navigationItems[this.newSlideIndex], 'cd-selected');
-	};
+            var url = document.location.href.toLowerCase();
+            k = -1;
+            var nLength = -1;
+            for (var i = 0; i < a.length; i++) {
+                if (url.indexOf(a[i].href.toLowerCase()) != -1 && a[i].href.length > nLength) {
+                    k = i;
+                    nLength = a[i].href.length;
+                }
+            }
 
-	HeroSlider.prototype.checkVideo = function() {
-		//check if a video outside the viewport is playing - if yes, pause it
-		var hiddenVideo = this.slides[this.oldSlideIndex].getElementsByTagName('video');
-		if( hiddenVideo.length ) hiddenVideo[0].pause();
+            if (k == -1 && /:\/\/(?:www\.)?[^.\/]+?\.[^.\/]+\/?$/.test) {
+                for (var i = 0; i < a.length; i++) {
+                    if (a[i].getAttribute("maptopuredomain") == "true") {
+                        k = i;
+                        break;
+                    }
+                }
+                if (k == -1 && a[0].getAttribute("maptopuredomain") != "false")
+                    k = 0;
+            }
 
-		//check if the select slide contains a video element - if yes, play the video
-		var visibleVideo = this.slides[this.newSlideIndex].getElementsByTagName('video');
-		if( visibleVideo.length ) visibleVideo[0].play();
-	};
+            if (k > -1) {
+                slip.style.width = items[k].offsetWidth + "px";
+                //slip.style.left = items[k].offsetLeft + "px";
+                sse1.move(items[k]); //comment out this line and uncomment the line above to disable initial animation
+            }
+            else {
+                slip.style.visibility = "hidden";
+            }
 
-	var heroSliders = document.getElementsByClassName("js-cd-hero");
-	if( heroSliders.length > 0 ) {
-		for( var i = 0; i < heroSliders.length; i++) {
-			(function(i){
-				new HeroSlider(heroSliders[i])
-			})(i);
-		}
-	}
+            for (var i = 0; i < items.length - 1; i++) {
+                items[i].onmouseover = function () {
+                    if (k == -1) slip.style.visibility = "visible";
+                    if (this.offsetLeft != slip.offsetLeft) {
+                        sse1.move(this);
+                    }
+                }
+            }
 
-	function removeClassPrefix(el, prefix) {
-		//remove all classes starting with 'prefix'
-        var classes = el.className.split(" ").filter(function(c) {
-            return c.indexOf(prefix) < 0;
-        });
-        el.className = classes.join(" ");
-	};
+            m.onmouseover = function () {
+                if (slip.t2)
+                    slip.t2 = clearTimeout(slip.t2);
+            };
 
-	//class manipulations - needed if classList is not supported
-	function hasClass(el, className) {
-	  	if (el.classList) return el.classList.contains(className);
-	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-	}
-	function addClass(el, className) {
-		var classList = className.split(' ');
-	 	if (el.classList) el.classList.add(classList[0]);
-	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
-	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
-	}
-	function removeClass(el, className) {
-		var classList = className.split(' ');
-	  	if (el.classList) el.classList.remove(classList[0]);	
-	  	else if(hasClass(el, classList[0])) {
-	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
-	  		el.className=el.className.replace(reg, ' ');
-	  	}
-	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
-	}
-$("nav select").change(function() {	
-  window.location = $(this).find("option:selected").val();
+            m.onmouseout = function () {
+                if (k > -1 && items[k].offsetLeft != slip.offsetLeft) {
+                    slip.t2 = setTimeout(function () { sse1.move(items[k]); }, 50);
+                }
+                if (k == -1) slip.t2 = setTimeout(function () { slip.style.visibility = "hidden"; }, 50);
+            };
+        },
+        move: function (target) {
+            clearInterval(slip.timer);
+            var direction = (slip.offsetLeft < target.offsetLeft) ? 1 : -1;
+            slip.timer = setInterval(function () { sse1.mv(target, direction); }, 15);
+        },
+        mv: function (target, direction) {
+            if (direction == 1) {
+                if (slip.offsetLeft - rebound < target.offsetLeft)
+                    this.changePosition(target, 1);
+                else {
+                    clearInterval(slip.timer);
+                    slip.timer = setInterval(function () {
+                        sse1.recoil(target, 1);
+                    }, 15);
+                }
+            }
+            else {
+                if (slip.offsetLeft + rebound > target.offsetLeft)
+                    this.changePosition(target, -1);
+                else {
+                    clearInterval(slip.timer);
+                    slip.timer = setInterval(function () {
+                        sse1.recoil(target, -1);
+                    }, 15);
+                }
+            }
+            this.changeWidth(target);
+        },
+        recoil: function (target, direction) {
+            if (direction == -1) {
+                if (slip.offsetLeft > target.offsetLeft) {
+                    slip.style.left = target.offsetLeft + "px";
+                    clearInterval(slip.timer);
+                }
+                else slip.style.left = slip.offsetLeft + 2 + "px";
+            }
+            else {
+                if (slip.offsetLeft < target.offsetLeft) {
+                    slip.style.left = target.offsetLeft + "px";
+                    clearInterval(slip.timer);
+                }
+                else slip.style.left = slip.offsetLeft - 2 + "px";
+            }
+        },
+        changePosition: function (target, direction) {
+            if (direction == 1) {
+                //following +1 will fix the IE8 bug of x+1=x, we force it to x+2
+                slip.style.left = slip.offsetLeft + Math.ceil(Math.abs(target.offsetLeft - slip.offsetLeft + rebound) / 10) + 1 + "px";
+            }
+            else {
+                //following -1 will fix the Opera bug of x-1=x, we force it to x-2
+                slip.style.left = slip.offsetLeft - Math.ceil(Math.abs(slip.offsetLeft - target.offsetLeft + rebound) / 10) - 1 + "px";
+            }
+        },
+        changeWidth: function (target) {
+            if (slip.offsetWidth != target.offsetWidth) {
+                var diff = slip.offsetWidth - target.offsetWidth;
+                if (Math.abs(diff) < 4) slip.style.width = target.offsetWidth + "px";
+                else slip.style.width = slip.offsetWidth - Math.round(diff / 3) + "px";
+            }
+        }
+    };
+} ();
+
+if (window.addEventListener) {
+    window.addEventListener("load", sse1.buildMenu, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent("onload", sse1.buildMenu);
+}
+else {
+    window["onload"] = sse1.buildMenu;
+}
+
+//Typing script
+
+
+    var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 175 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2.5; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #ffffff }";
+  document.body.appendChild(css);
+};
+
+//slidtoggle
+$(document).ready(function(){
+    $("img").click(function(){
+        $("#sse1").slideToggle(1000);
+    });
 });
-})();
-
